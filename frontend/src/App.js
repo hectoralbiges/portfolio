0f23b4111d77e89aa7505558b28f34d299fb31db
Hector <hector.albiges@random.studio>
2026-05-24 16:21:59 +0200

    add background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Album from "./pages/Album";
 
+import bg1 from "./assets/bg/1.jpg";
+import bg2 from "./assets/bg/2.jpg";
+import bg3 from "./assets/bg/3.jpg";
+import bg4 from "./assets/bg/4.jpg";
+
 function App() {
+  const [bg, setBg] = useState(bg4);
+
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/album/:id" element={<Album />} />
-        <Route path="*" element={<div>404 - Page not found</div>} />
-      </Routes> 
+      <div
+        className="app"
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "100vh",
+        }}
+      >
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/album/:id" element={<Album />} />
+          <Route path="*" element={<div>404 - Page not found</div>} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
